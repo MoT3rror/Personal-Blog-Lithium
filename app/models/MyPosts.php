624494f62ed76b6a7b7b7bb5df1ad3c2315fa973
MoT3rror <jbixler@etccomputerland.com>
2013-08-26 22:22:23 -0500

    Styling and Model work
    Got some style done on home and working on getting model more complex

--- app/models/MyPosts.php
+++ app/models/MyPosts.php
@@ -4,6 +4,23 @@ namespace app\models;
 
 class MyPosts extends \lithium\data\Model
 {
-
+  protected $_scheme = array(
+      'id'    => array(
+          'type'    => 'id',
+          'length'  => 10,
+          'null'    => false,
+          'default' => null,
+      ),
+      'title' => array(
+          'type'    => 'string',
+          'length'  => 255,
+          'null'    => false,
+          'default' => null,
+      ),
+      'body'  => array(
+          'type'    => 'text',
+          'null'    => true,
+      ),
+  );
 }
 ?>
--- app/views/pages/home.html.php
+++ app/views/pages/home.html.php
@@ -10,9 +10,40 @@ $this->title('Home');
 $this->html->style('debug', array('inline' => false));
 
 ?>
-<h4 class="pagination-centered">Software developer</h4>
+<h4>Software developer</h4>
 
-<?php foreach($myPosts as $post): ?>
-<div><?=$post->title ?></div>
-<div><?=$post->body ?></div>
-<?php endforeach; ?>
+<div class="about">
+  Looking for an experience web developer to work on your website or part of a team? Then you have come to the right place.
+  <br />
+  My experience ranges in:
+  <ul>
+    <li>HTML/XHTML/HTML 5</li>
+    <li>CSS</li>
+    <li>Javascript</li>
+    <li>JQuery</li>
+    <li>PHP 5.3</li>
+    <li>Lithium Framework</li>
+    <li>MVC frameworks</li>
+    <li>OOP in PHP</li>
+    <li>PHP Unit testing</li>
+    <li>SVN/Github</li>
+    <li>MySQL</li>
+    <li>Database Schemes</li>
+    <li>Rational Database</li>
+    <li>Jenkins</li>
+    <li>Ant Build Process</li>
+  </ul>
+
+  This is the short list. <a href="$this->url('Work::index')">View my previous projects</a> or <a href="$this->url('Pages::resume')">View my resume</a>
+</div>
+
+<div class="posts">
+<?php 
+foreach($myPosts as $post): ?>
+  <hr />
+  <div class="post">
+    <h4><?=$post->title ?></h4>
+    <div class="body"><?=$post->body ?></div>
+  </div>
+<?php 
+endforeach; ?>
